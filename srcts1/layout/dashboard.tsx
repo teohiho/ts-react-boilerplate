d118adb835a880b9f8fc6e45d4c30f4d796517cb
Zaku <tomle.zaku@gmail.com>
2018-06-11 12:03:01 +0700

    change structure

---
+++ srcts1/layout/dashboard.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+export interface DashBoardProps {
+  
+}
+
+export interface DashBoardState {
+}
+
+export default class DashBoard extends React.Component<DashBoardProps, DashBoardState> {
+  constructor(props: DashBoardProps) {
+    super(props);
+
+      this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
