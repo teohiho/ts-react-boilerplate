fbbfd7b52e2d2624ad7adf78db102d3afee5fd6d
Zaku <tomle.zaku@gmail.com>
2018-06-20 16:49:05 +0700

    Update reducer test

--- src/module/todo/logic.redux/__test__/reducer.test.ts
+++ src/module/todo/logic.redux/__test__/reducer.test.ts
@@ -8,4 +8,24 @@ describe('todos reducer', () => {
       initialState,
     )
   })
+  it(`should handle ${actionType.ADD_TASK}`, () => {
+    const sampleTest = {
+      tags: [''],
+      title: 'Test',
+      completed: false,
+      id: '1-test',
+    }
+    expect(
+      reducer(undefined, {
+        type: actionType.ADD_TASK,
+        payload: sampleTest,
+      }),
+    ).toEqual({
+      ...initialState,
+      tasks: [
+        ...initialState.tasks,
+        sampleTest,
+      ],
+    })
+    })
 })
