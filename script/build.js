4d0661d93e6150d64299e069faf4b5bf0845e3f9
Zaku <tomle.zaku@gmail.com>
2018-07-25 12:27:31 +0700

    fix conflict

--- script/build.js
+++ script/build.js
@@ -1,4 +1,7 @@
 'use strict';
+// Load env by default
+const { settingEnv } = require('../conf/helper/env')
+settingEnv('production')
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
