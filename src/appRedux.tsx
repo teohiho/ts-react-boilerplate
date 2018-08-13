79c5f6c26ee12b0c1de11400af08cc0090034ed9
Zaku <tomle.zaku@gmail.com>
2018-08-13 17:47:05 +0700

    rename files

---
+++ src/appRedux.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import configureStore from 'conf/redux/redux'
+import { branch, compose, lifecycle, pure, renderComponent, withStateHandlers } from 'recompose'
+import { AppConfig } from './appConfig'
+import { Loading } from './com/index'
+const { store, persistor } = configureStore()
+interface IAppReduxPropsOut {
+
+}
+interface IAppReduxState {
+	isLoading: boolean
+}
+
+interface IAppReduxPropsIn extends IAppReduxPropsOut, IAppReduxState {
+
+}
+
+const ReduxView = () => (
+	<Provider store={store}>
+		<PersistGate loading={<Loading/>} persistor={persistor} >
+			<AppConfig />
+		</PersistGate>
+	</Provider>
+)
+
+
+const withLoading =	branch(
+		({ isLoading }) => isLoading,
+		renderComponent(Loading),
+	)
+
+const LoadingStateHandler = withStateHandlers(
+	{
+		isLoading: true,
+	},
+	{
+		loadSuccessful: () => () => ({ isLoading: false }),
+	},
+)
+
+const withLifeCycle = lifecycle({
+	componentDidMount() {
+		// this.setState({ isLoading: false })
+		setTimeout(() => this.setState({ isLoading: false }), 500)
+	},
+})
+
+
+export const AppRedux = compose(pure, LoadingStateHandler, withLifeCycle, withLoading)(ReduxView)
