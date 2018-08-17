0d201164528265a5e1a0f38279e11f9eb58d795f
phannerl <phanman7@gmail.com>
2018-08-17 10:50:09 +0700

    feature: add img

--- src/app/man/page/man.tsx
+++ src/app/man/page/man.tsx
@@ -1,4 +1,4 @@
-import { Alignment, AnchorButton, Button, ButtonGroup, FormGroup, InputGroup, Navbar, Tab , Tabs } from '@blueprintjs/core'
+import { Alignment, AnchorButton, Button, ButtonGroup, FormGroup, InputGroup, Navbar , Tabs } from '@blueprintjs/core'
 import { createTab } from 'com/index'
 import * as React from 'react'
 import { Motion, spring, TransitionMotion } from 'react-motion'
@@ -18,16 +18,6 @@ const SettingPageView = () => (
 					<InputGroup id="text-input" className={`${style.nav}__input`} placeholder="Placeholder text" />
 				</FormGroup>
 			</div>
-			<div className={`${style.nav}__right`}>
-				<Tabs id="a">
-					<Tab id="b" title="Become a host" panel={<h1>Become a host</h1>} />
-					<Tab id="c" title="Earn credit" panel={<h1>Earn credit</h1>} />
-					<Tab id="e" title="Help" panel={<h1>Help</h1>} />
-					<Tab id="f" title="Sign up" panel={<h1>Sign up</h1>} />
-					<Tab id="d" title="Log in" panel={<h1>Log in</h1>} />
-					<Tabs.Expander />
-				</Tabs>
-			</div>
 		</div>
 		<br/>
 		<ButtonGroup minimal={true} >
@@ -37,55 +27,48 @@ const SettingPageView = () => (
 		</ButtonGroup>
 	</>
 )
+// <=====================================================================================>
 
-const Special3 = () => (<h1>Abc</h1>)
+const styleById = (id: number) => {
+	switch (id) {
+		case 1: return 0
+		default: return id * 100
+	}
+}
 
-const openStateHandler = withStateHandlers(
+// <=====================================================================================>
+
+
+interface ITabsState {
+	idSelected: number
+}
+interface ITabsStateHandler {
+	changeId: (id: string) => void
+}
+const idStateHandler = withStateHandlers(
 	{
-		idSelected: '1',
+		idSelected: 1,
 	},
 	{
-		changeId: () => (id: string) => ({ idSelected: id }),
-		// handleMouseDown: ({ id }) => () => ({ id: !id }),
-		// handleTouchStart: ({ id }) => (e: React.TouchEvent<HTMLButtonElement>) => {
-		// 	e.preventDefault()
-		// 	return { id: !id }
-		// },
+		changeId: () => (id: number) => ({ idSelected: id }),
 	},
 )
-interface IDemoViewPropsOut {}
-interface IDemoState {
-	idSelected: string
-}
-interface IDemoHandler {
-	changeId: (id: string) => void
-	// handleMouseDown: () => void
-	// handleTouchStart: (e: React.TouchEvent<HTMLButtonElement>) => void
-}
-interface IDemoViewPropsIn extends IDemoViewPropsOut, IDemoState, IDemoHandler {}
-const DemoView = ({ idSelected, changeId }: IDemoViewPropsIn) => (
-	<div>
-		<button className="demo0-button"
-			onClick={() => changeId('1')}
-		>
-			Tab1
-		</button>
-		<button className="demo0-button"
-			onClick={() => changeId('2')}
-		>
-			Tab2
-		</button>
-		<button className="demo0-button"
-			onClick={() => changeId('3')}
-		>
-			Tab3
-		</button>
-		<button className="demo0-button"
-			onClick={() => changeId('4')}
-		>
-			Tab4
-		</button>
 
+interface ITab {
+	id: number
+	title: string
+}
+
+interface ITabsPropsOut {
+	tabs: ITab[]
+}
+interface ITabsPropsIn extends ITabsStateHandler, ITabsPropsOut, ITabsState {}
+
+const TabView = ({ changeId, tabs, idSelected }: ITabsPropsIn) => (
+	<>
+		{tabs.map(({ title, id }) => (
+			<button className="demo0-button" key={id} onClick={() => changeId(`${id}`)}>{title}</button>
+		))}
 		<Motion style={{ x: spring(styleById(idSelected)) }}>
 			{({ x }) =>
 			// children is a callback which should accept the current value of
@@ -98,70 +81,57 @@ const DemoView = ({ idSelected, changeId }: IDemoViewPropsIn) => (
 			</div>
 			}
 		</Motion>
-	</div>
+	</>
 )
-const styleById = (id: string) => {
-	switch (id) {
-		case '1': return 0
-		case '2': return 100
-		case '3': return 200
-		default: return 300
-	}
-}
-// <=====================================================================================>
-const idStateHandler = withStateHandlers(
-	([{
-			idSelect:'1',
-			title:'Tab1',
-		},
-		{
-			idSelect:'2',
-			title:'Tab2',
-	}]
-	),
-	{
-		changeId: () => (id: string) => ({ idSelect: id }),
-	},
-)
-interface IDemoViewPropsOut {}
-interface IDemoState {
-	idSelect: string,
-	title: string,
-}
-interface IDemoHandler {
-	changeId: (id: string) => void
-}
-interface IDemoViewPropsIn extends IDemoViewPropsOut, IDemoState, IDemoHandler {}
 
-const DemoViewA = ({ idSelect, changeId }: IDemoViewPropsIn) => (
-	<div>
-		<button className="demo0-button"
-			onClick={() => changeId('1')}
-		>
-			Tab1
-		</button>
+const Tab = compose<ITabsPropsIn, ITabsPropsOut>(idStateHandler)(TabView)
 
-
-		<Motion style={{ x: spring(styleById(idSelect)) }}>
-			{({ x }) =>
-			// children is a callback which should accept the current value of
-			// `style`
-			<div className="demo0">
-				<div className="demo0-block" style={{
-				WebkitTransform: `translate3d(${x}px, 0, 0)`,
-				transform: `translate3d(${x}px, 0, 0)`,
-				}} />
-			</div>
+const DemoAView = () => (
+	<>
+		<Tab
+			tabs={
+				[
+					{
+						id: 0,
+						title: 'Tab1',
+					},
+					{
+						id: 1,
+						title: 'Tab2',
+					},
+					{
+						id: 2,
+						title: 'Tab3',
+					},
+					{
+						id: 3,
+						title: 'Tab4',
+					},
+					{
+						id: 4,
+						title: 'Tab5',
+					},
+					{
+						id: 5,
+						title: 'Tab6',
+					},
+					{
+						id: 6,
+						title: 'Tab7',
+					},
+				]
 			}
-		</Motion>
-	</div>
+		/>
+	</>
 )
 
-const Demo = compose<IDemoViewPropsIn, IDemoViewPropsOut>(openStateHandler)(DemoView)
-const DemoA = compose<IDemoViewPropsIn, IDemoViewPropsOut>(openStateHandler)(DemoViewA)
 
+const DemoA = compose()(DemoAView)
+// <=====================================================================================>
 const Special4 = () => (<h1>Abc</h1>)
+// <=====================================================================================>
 const Special5 = () => (<h1>Abc</h1>)
+// <=====================================================================================>
 
 const AddTab = createTab({
 	breadcrumbItems: [
@@ -180,14 +150,14 @@ const AddTab = createTab({
 			panel: <DemoA />,
 			title: 'Become a host',
 		},
-		{
-			id: 'Earn credit',
-			panel: <Demo />,
-			title: 'Earn credit',
-		},
+		// {
+		// 	id: 'Earn credit',
+		// 	panel: <Demo />,
+		// 	title: 'Earn credit',
+		// },
 		{
 			id: 'Help',
-			panel: <Special3 />,
+			panel: <SettingPageView />,
 			title: 'Help',
 		},
 		{
